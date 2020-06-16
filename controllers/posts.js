0495e22af23a047d77c9ebc353741eb8332d9073
Vineet Joshi <vineet0713@gmail.com>
2020-06-16 12:21:18 -0700

    Add routes for editing a post and for retrieving a single post by id.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,18 +1,6 @@
 const Post = require('./../models/post');
 
 exports.getPosts = (request, response, next) => {
-	const posts = [
-		{
-			id: 'dummyid1',
-			title: 'Dummy Title 1',
-			content: 'Dummy Content 1',
-		},
-		{
-			id: 'dummyid2',
-			title: 'Dummy Title 2',
-			content: 'Dummy Content 2',
-		},
-	];
 	Post.find()
 		.then(fetchedPosts => {
 			response.status(200).json({
@@ -42,9 +30,36 @@ exports.deletePost = (request, response, next) => {
 	const idOfPostToDelete = request.params.postid;
 	Post.deleteOne({ _id: idOfPostToDelete })
 		.then(result => {
-			console.log(result);
 			response.status(200).json({
 				message: 'success!',
+			});
+		})
+		.catch(error => next(error));
+};
+
+exports.updatePost = (request, response, next) => {
+	const idOfPostToUpdate = request.params.postid;
+	const updatedPost = new Post({
+		_id: idOfPostToUpdate,
+		title: request.body.title,
+		content: request.body.content,
+	});
+	Post.updateOne({ _id: idOfPostToUpdate }, updatedPost)
+		.then(result => {
+			response.status(200).json({
+				message: 'success!',
+			});
+		})
+		.catch(error => next(error));
+};
+
+exports.getPost = (request, response, next) => {
+	const idOfPostToGet = request.params.postid;
+	Post.findById(idOfPostToGet)
+		.then(fetchedPost => {
+			response.status(200).json({
+				message: 'success!',
+				post: fetchedPost,
 			});
 		})
 		.catch(error => next(error));
--- routes/posts.js
+++ routes/posts.js
@@ -9,7 +9,13 @@ router.get('/posts', postsController.getPosts);
 // POST api/post
 router.post('/post', postsController.storePost);
 
-// DELETE api/post
+// DELETE api/post/:postid
 router.delete('/post/:postid', postsController.deletePost);
+
+// PUT api/post/:postid
+router.put('/post/:postid', postsController.updatePost);
+
+// GET api/post/:postid
+router.get('/post/:postid', postsController.getPost);
 
 module.exports = router;
